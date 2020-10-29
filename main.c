f796982d95d08a545a0d03ac0c37231746e65011
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-29 15:31:11 +0900

    실습2

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define size5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	//array declaration
+	
 	int grade[5];
-	int i;
+	int i, average, sum;
 	
 	//value input
 	grade[0] = 10;
@@ -16,6 +17,9 @@ int main(int argc, char *argv[]) {
 	grade[4] = 50;
 	
 	for ( i=0; i<5; i++)
-	  printf("student %i's grade : %i\n", i, grade[i]);
+	sum=sum+grade[i];
+	average=sum/5;
+	
+	  printf("the average is : %i\n", average);
 	return 0;
 }
